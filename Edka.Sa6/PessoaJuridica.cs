dc3ca84e604cc6324b418ee3a85225c69eea385b
geovaneviana.df@gmail.com <geovaneviana.df@gmail.com>
2022-11-01 09:17:06 -0300

    Desafio OO

---
+++ Edka.Sa6/PessoaJuridica.cs
@@ -0,0 +1,19 @@
+﻿using OOEdka.Sa6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edka.Sa6 {
+    class PessoaJuridica : Cliente
+    {
+        public string CNPJ { get; set; }
+
+        public void Mostrar() {
+
+            base.Mostrar();
+            Console.WriteLine("CNPJ: {0}", CNPJ);
+        }
+    }
+}
---
+++ Edka.Sa6/Program.cs
@@ -0,0 +1,31 @@
+﻿using Edka.Sa6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOEdka.Sa6 {
+     class Program {
+        static void Main(string[] args) {
+
+           PessoaFisica pf = new PessoaFisica();
+            pf.Codigo = 2724;
+            pf.Nome = "Geovane";
+            pf.CPF = "023.456.789-10";
+
+            pf.Mostrar();
+            Console.WriteLine("________________________________");
+
+            PessoaJuridica pj = new PessoaJuridica();
+            pj.Codigo = 3567;
+            pj.Nome = "New Project";
+            pj.CNPJ = "68.895.586/0005-41";
+
+            pj.Mostrar();
+
+            Console.ReadKey(); 
+            
+        }
+    }
+}
