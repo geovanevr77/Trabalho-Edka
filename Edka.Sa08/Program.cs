8692580e67e1cac5a502fb6a9bcb25972c61a378
geovaneviana.df@gmail.com <geovaneviana.df@gmail.com>
2022-11-01 12:19:26 -0300

    Atualizacao1

---
+++ Edka.Sa08/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sistema;
+
+
+namespace Edka.Sa08 {
+    class Program {
+        static void Main(string[] args) {
+
+            SistemaBanco conta = new SistemaBanco(TipoConta.ContaCorrente, "123456");
+            Console.WriteLine("Mostrar saldo: ");
+            conta.Num = "123";
+            conta.Depositar(800);
+            conta.Sacar(300);
+            Console.WriteLine(conta.GetSaldo().ToString());
+            Console.ReadLine();
+           
+            
+         
+        }
+    }
+}
---
+++ Edka.Sa08/SistemaBanco.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sistema
+    {
+    class SistemaBanco {
+
+        public enum TipoConta { ContaCorrente, ContaPoupanca };
+        public class conta {
+
+            public string Num;
+            private double _saldo;
+            public TipoConta Tipo;
+
+
+            public conta(TipoConta tipo, string num) {
+
+                this.Num = num;
+                this.Tipo = tipo;
+                _saldo = 0;
+
+            }
+            public void Sacar(double valor) {
+                _saldo = _saldo - valor;
+            }
+
+            public void Depositar(double valor) {
+                _saldo = _saldo + valor;
+            }
+            public double GetSaldo() {
+                return _saldo;
+            }
+        }
+    }
+}
